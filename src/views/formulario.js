81124c4abaf2df91926c35bf54befe21313ffe73
Nicolas Cisterna <nicolascisterna729@gmail.com>
2021-03-26 15:48:57 -0300

    rescatar valores formulario

--- src/views/formulario.js
+++ src/views/formulario.js
@@ -1,3 +1,5 @@
+
+import React, { useState } from 'react';
 import FormControl from '@material-ui/core/FormControl';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
@@ -21,64 +23,81 @@ function Formulario() {
     }));
 
     const classes = useStyles();
+
+
+    // Detectar los cambios realizados en los input 
+
+    const valorInputs = (event) => {
+        setDatoForm({
+            ...datosForm,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const agregarAcceso = (event) => {
+        event.preventDefault()
+    }
     return (
         <>
             <Typography variant="h3" gutterBottom>
                 Control de Acceso
             </Typography>
             <Grid container spacing={3} direction="row">
-                <Grid item xs={6} >
-                    <FormControl>
-                        <InputLabel htmlFor="rut">Rut</InputLabel>
-                        <Input id="rut" aria-describedby="my-helper-rut" />
-                        {/* <FormHelperText id="my-helper-rut">Si el rut termina en (K), reemplácelo por un cero (0)</FormHelperText> */}
-                    </FormControl>
+                <Form onSubmit={agregarAcceso}>
 
-                </Grid>
-                <Grid item xs={6}>
-                    <FormControl>
-                        <InputLabel htmlFor="nombre">Nombre</InputLabel>
-                        <Input id="nombre" />
-                    </FormControl>
-                </Grid>
+                    <Grid item xs={6} >
+                        <FormControl>
+                            <InputLabel htmlFor="rut">Rut</InputLabel>
+                            <Input id="rut" aria-describedby="my-helper-rut" />
+                            {/* <FormHelperText id="my-helper-rut">Si el rut termina en (K), reemplácelo por un cero (0)</FormHelperText> */}
+                        </FormControl>
 
-
-                <Grid item xs={6}>
-                    <FormControl>
-                        <InputLabel htmlFor="vehiculo">Vehículo</InputLabel>
-                        <Input id="vehiculo" />
-                    </FormControl>
-                </Grid>
-                <Grid item xs={6}>
-                    <FormControl>
-                        <InputLabel htmlFor="temperatura">Temperatura</InputLabel>
-                        <Input id="temperatura" />
-                    </FormControl>
-                </Grid>
-                <Grid item xs={6}>
-                    <FormControl>
-                        <InputLabel htmlFor="patente">Patente</InputLabel>
-                        <Input id="patente" />
-                    </FormControl>
-                </Grid>
-                <Grid item xs={6}>
-                    <FormControl>
-                        <InputLabel htmlFor="observaciones">Observaciones</InputLabel>
-                        <Input
-                            id="observaciones"
-                            multiline
-                            rows={4} />
-                    </FormControl>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <FormControl>
+                            <InputLabel htmlFor="nombre">Nombre</InputLabel>
+                            <Input id="nombre" onChange={valorInputs} />
+                        </FormControl>
+                    </Grid>
 
 
-                </Grid>
+                    <Grid item xs={6}>
+                        <FormControl>
+                            <InputLabel htmlFor="vehiculo">Vehículo</InputLabel>
+                            <Input id="vehiculo" onChange={valorInputs} />
+                        </FormControl>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <FormControl>
+                            <InputLabel htmlFor="temperatura">Temperatura</InputLabel>
+                            <Input id="temperatura" onChange={valorInputs} />
+                        </FormControl>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <FormControl>
+                            <InputLabel htmlFor="patente">Patente</InputLabel>
+                            <Input id="patente" onChange={valorInputs} />
+                        </FormControl>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <FormControl>
+                            <InputLabel htmlFor="observaciones">Observaciones</InputLabel>
+                            <Input
+                                id="observaciones"
+                                multiline
+                                rows={4} onChange={valorInputs} />
+                        </FormControl>
 
-                <Button variant="contained" color="primary">
-                    <SearchIcon />   BUSCAR
+
+                    </Grid>
+
+                    <Button variant="contained" color="primary">
+                        <SearchIcon />   BUSCAR
 
 </Button>
 
 
+                </Form>
 
             </Grid>
 
